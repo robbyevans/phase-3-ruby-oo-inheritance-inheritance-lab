bc65513dad590111136e2b2776dd4b2a644f1ba2
Evans Rutto <robbyevans001@gmail.com>
2022-08-25 22:38:05 +0300

    commit tree 36fe208b53568217baa182025b5a48381453ff27

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+    
+    @knowledge=[]
+
+    def initialize
+         @knowledge=[]        
+    end
+
+    def knowledge
+        @knowledge
+    end    
+    
+    def learn(string)
+        @knowledge << string
+    end    
 
 end
